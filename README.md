59509bba610b27b2f3a9bde0bdadfaa714daa694
Jonathan Piedrasanta <jonathanpiedrasanta@Jonathans-MacBook-Pro.local>
2019-04-22 00:45:22 -0600

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-This project was bootstrapped with [Create React App]
-
-React app UI for list of movies
---
+++ precache-manifest.3cf8d9175baac54eb4799897cf74634c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39bbd105fb886f331a82",
+    "url": "/movieApp/static/css/main.d03b606e.chunk.css"
+  },
+  {
+    "revision": "39bbd105fb886f331a82",
+    "url": "/movieApp/static/js/main.a7400422.chunk.js"
+  },
+  {
+    "revision": "abf7d65e4eddecf4e34f",
+    "url": "/movieApp/static/js/runtime~main.4a8bc2e3.js"
+  },
+  {
+    "revision": "1b39017316a8e31336a2",
+    "url": "/movieApp/static/css/2.ee740695.chunk.css"
+  },
+  {
+    "revision": "1b39017316a8e31336a2",
+    "url": "/movieApp/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "c223b23686044affafbab69193db6238",
+    "url": "/movieApp/index.html"
+  }
+];
